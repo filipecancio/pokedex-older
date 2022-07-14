2d36db597d934f93b1bb66a6044fabde9e6f1a12
Filipe Câncio <filipe.cancio@gmail.com>
2022-07-13 22:15:26 -0300

    criando fragments

--- app/src/main/java/dev/cancio/pokedex/ui/activity/MainActivity.kt
+++ app/src/main/java/dev/cancio/pokedex/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package dev.cancio.pokedex
+package dev.cancio.pokedex.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.cancio.pokedex.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
